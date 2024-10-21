bfb8a41bc60a8b323713a74cb95b0f07254acccb
Beboto <bebotoghosh10@gmail.com>
2024-10-21 20:32:35 +0530

    Final Build 4 Done

--- src/firebase.js
+++ src/firebase.js
@@ -4,7 +4,7 @@ import { getAuth } from "firebase/auth";
 import { getStorage } from "firebase/storage";
 import { getFirestore } from "firebase/firestore";
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_apikey,
+  apiKey: process.env.NEXT_PUBLIC_apiKey,
   authDomain: process.env.NEXT_PUBLIC_authDomain,
   projectId: process.env.NEXT_PUBLIC_projectId,
   storageBucket: process.env.NEXT_PUBLIC_storageBucket,
