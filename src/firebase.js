722db1be5db5ec520d790d8be2331f5b1be51f0d
Beboto <bebotoghosh10@gmail.com>
2024-10-21 19:10:08 +0530

    Project files uploaded

---
+++ src/firebase.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getStorage } from "firebase/storage";
+import { getFirestore } from "firebase/firestore";
+const firebaseConfig = {
+  apiKey: process.env.NEXT_PUBLIC_apikey,
+  authDomain: process.env.NEXT_PUBLIC_authDomain,
+  projectId: process.env.NEXT_PUBLIC_projectId,
+  storageBucket: process.env.NEXT_PUBLIC_storageBucket,
+  messagingSenderId: process.env.NEXT_PUBLIC_apikey,
+  appId: process.env.NEXT_PUBLIC_apikey,
+  measurementId: process.env.NEXT_PUBLIC_apikey
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const storage = getStorage(app);
+export const db = getFirestore(app);
+
